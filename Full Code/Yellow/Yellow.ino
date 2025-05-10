68db2777c01ddb473f72125958bfed6541279b4a
Ceyhan <ceyhanyildizt3@gmail.com>
2025-05-10 15:45:21 +0200

    Update Yellow.ino

--- Full Code/Yellow/Yellow.ino
+++ Full Code/Yellow/Yellow.ino
@@ -1,4 +1,3 @@
-// Includes
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include <XPT2046_Touchscreen.h>
@@ -6,11 +5,11 @@
 #include <HTTPClient.h>
 
 // Touchscreen pins
-#define XPT2046_IRQ 36   // T_IRQ
-#define XPT2046_MOSI 32  // T_DIN
-#define XPT2046_MISO 39  // T_OUT
-#define XPT2046_CLK 25   // T_CLK
-#define XPT2046_CS 33    // T_CS
+#define XPT2046_IRQ 36
+#define XPT2046_MOSI 32
+#define XPT2046_MISO 39
+#define XPT2046_CLK 25
+#define XPT2046_CS 33
 
 // Display + UI settings
 #define SCREEN_WIDTH 320
@@ -19,10 +18,9 @@
 #define DATA_FONT_SIZE 4
 
 // Wi-Fi credentials
-const char* ssid = "504F94A12106";
-const char* password = "";
-const char* serverURL = "http://192.168.1.120/sensdata";  // Replace with your ESP8266's IP and endpoint
-
+const char* ssid = "telenet-Ster";
+const char* password = "0485541209n";
+const char* serverURL = "http://192.168.0.223/sensdata";  // Replace with your ESP8266 IP
 
 // Globals
 TFT_eSPI tft = TFT_eSPI();  // Uses config from User_Setup.h
@@ -32,12 +30,14 @@ XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
 // Sensor Data
 float temperature = 0.0f;
 float humidity = 0.0f;
-unsigned int soundLevel = 0;  // New: peakToPeak mic data
+unsigned int soundLevel = 0;
+float ambiTemperature = 0.0f;
+float ambiHumidity = 0.0f;
 
 // Page Navigation
-int currentPage = 0;  // 0 = DHT11, 1 = MAX4466
+int currentPage = 0;  // 0 = DHT11, 1 = Sound, 2 = AmbiMate
 
-// --- UI Drawing Functions ---
+// UI Drawing
 void drawBackground() {
   tft.fillScreen(TFT_DARKGREY);
   tft.drawLine(0, 31, SCREEN_WIDTH, 31, TFT_BLACK);
@@ -46,13 +46,11 @@ void drawBackground() {
 
 void drawDHT11Page() {
   drawBackground();
-
   tft.setTextColor(TFT_BLACK);
   tft.setTextSize(GENERAL_FONT_SIZE);
   tft.drawString("KY-015_DHT11", 31, 6);
   tft.setTextColor(TFT_LIGHTGREY);
   tft.drawString("KY-015_DHT11", 30, 5);
-
   tft.fillCircle(300, 15, 6, TFT_GREEN);
 
   tft.setTextColor(TFT_BLACK);
@@ -63,13 +61,11 @@ void drawDHT11Page() {
 
 void drawMAX4466Page() {
   drawBackground();
-
   tft.setTextColor(TFT_BLACK);
   tft.setTextSize(GENERAL_FONT_SIZE);
   tft.drawString("MAX_4466", 31, 6);
   tft.setTextColor(TFT_LIGHTGREY);
   tft.drawString("MAX_4466", 30, 5);
-
   tft.fillCircle(300, 15, 6, TFT_GREEN);
 
   tft.setTextColor(TFT_BLACK);
@@ -78,19 +74,32 @@ void drawMAX4466Page() {
   tft.drawString(String(soundLevel) + " units", 30, 140);
 }
 
-// --- Setup ---
+void drawAmbiMatePage() {
+  drawBackground();
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(GENERAL_FONT_SIZE);
+  tft.drawString("AmbiMate MS4", 31, 6);
+  tft.setTextColor(TFT_LIGHTGREY);
+  tft.drawString("AmbiMate MS4", 30, 5);
+  tft.fillCircle(300, 15, 6, TFT_GREEN);
+
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(DATA_FONT_SIZE);
+  tft.drawString("Temp: " + String(ambiTemperature, 1) + " C", 30, 60);
+  tft.drawString("Hum: " + String(ambiHumidity, 1) + " %", 30, 120);
+}
+
+// Setup
 void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
 
-  // Init touchscreen SPI
   touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
   touchscreen.begin(touchscreenSPI);
   touchscreen.setRotation(1);
 
-  // Init screen
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
@@ -98,71 +107,85 @@ void setup() {
   tft.setTextSize(2);
   tft.setCursor(20, 100);
   tft.println("Touch screen to begin...");
-
   delay(1500);
 }
 
-// --- Fetch Sensor Data from ESP8266 ---
+// Fetch Sensor Data from HTTP JSON
 void fetchSensorData() {
   HTTPClient http;
   http.begin(serverURL);
-
-  int httpCode = http.GET(); // Send GET request
+  int httpCode = http.GET();
 
   if (httpCode > 0) {
     String payload = http.getString();
 
-    // Parse the JSON data
+    // DHT11
     int tempStart = payload.indexOf("\"Temperature\":") + 14;
     int tempEnd = payload.indexOf(",", tempStart);
     temperature = payload.substring(tempStart, tempEnd).toFloat();
 
     int humStart = payload.indexOf("\"Humidity\":") + 11;
-    int humEnd = payload.indexOf(",", humStart);  // CHANGED: because we now have 3 fields
+    int humEnd = payload.indexOf(",", humStart);
     humidity = payload.substring(humStart, humEnd).toFloat();
 
+    // Sound
     int soundStart = payload.indexOf("\"SoundLevel\":") + 13;
-    int soundEnd = payload.indexOf("}", soundStart);
+    int soundEnd = payload.indexOf(",", soundStart);
     soundLevel = payload.substring(soundStart, soundEnd).toInt();
+
+    // AmbiMate Temp
+    int ambiTempStart = payload.indexOf("\"AmbiMateTemperature\":") + 22;
+    int ambiTempEnd = payload.indexOf(",", ambiTempStart);
+    ambiTemperature = payload.substring(ambiTempStart, ambiTempEnd).toFloat();
+
+    // AmbiMate Hum
+    int ambiHumStart = payload.indexOf("\"AmbiMateHumidity\":") + 20;
+    int ambiHumEnd = payload.indexOf("}", ambiHumStart);
+    ambiHumidity = payload.substring(ambiHumStart, ambiHumEnd).toFloat();
   }
 
   http.end();
 }
 
+// Loop
 unsigned long lastFetchTime = 0;
-const unsigned long fetchInterval = 5000;  // 5 seconds interval between fetch requests
+const unsigned long fetchInterval = 5000;
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  if (touchscreen.tirqTouched() && touchscreen.touched()) {
-    TS_Point point = touchscreen.getPoint();
+  // Periodic fetch
+  if (currentMillis - lastFetchTime >= fetchInterval) {
+    fetchSensorData();
+    lastFetchTime = currentMillis;
 
-    // Map touch point
-    int x = map(point.x, 200, 3700, 1, SCREEN_WIDTH);
-    int y = map(point.y, 240, 3800, 1, SCREEN_HEIGHT);
-
-    // Switch pages if screen touched
-    if (y > SCREEN_HEIGHT / 2) {
-      currentPage = (currentPage + 1) % 2;  // Only 2 pages (0 and 1)
-    }
-    else {
-      currentPage = (currentPage + 1) % 2;
-    }
-
-    // Fetch new sensor data if enough time passed
-    if (currentMillis - lastFetchTime >= fetchInterval) {
-      fetchSensorData();
-      lastFetchTime = currentMillis;
-    }
-
-    // Draw the correct page
     if (currentPage == 0) {
       drawDHT11Page();
     } else if (currentPage == 1) {
       drawMAX4466Page();
+    } else if (currentPage == 2) {
+      drawAmbiMatePage();
     }
   }
 
-  delay(50);  // Small delay for touch responsiveness
+  // Touch page switch
+  if (touchscreen.tirqTouched() && touchscreen.touched()) {
+    TS_Point point = touchscreen.getPoint();
+    int x = map(point.x, 200, 3700, 1, SCREEN_WIDTH);
+    int y = map(point.y, 240, 3800, 1, SCREEN_HEIGHT);
+
+    currentPage = (currentPage + 1) % 3;
+
+    if (currentPage == 0) {
+      drawDHT11Page();
+    } else if (currentPage == 1) {
+      drawMAX4466Page();
+    } else if (currentPage == 2) {
+      drawAmbiMatePage();
+    }
+
+    delay(300);  // Debounce
+  }
+
+  delay(50);
 }
